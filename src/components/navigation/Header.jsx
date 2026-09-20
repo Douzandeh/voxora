45f2a91224ced059bb04ddff957a448ad974ad62
Hossein Douzandeh <douzandeh.k@gmail.com>
2026-09-20 20:47:37 +0330

    feat: create header section for use react bootstrap

--- src/components/navigation/Header.jsx
+++ src/components/navigation/Header.jsx
@@ -1,6 +1,55 @@
 import "./navigation.css";
+
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Container>
+        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary mb-3">
+          <Container fluid>
+            <Navbar.Brand href="#">Voxora</Navbar.Brand>
+
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-lg`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
+                  Voxora
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="me-auto">
+                  <Nav.Link href="#action1">Home</Nav.Link>
+                  <Nav.Link href="#action2">Shop</Nav.Link>
+                  <Nav.Link href="#action2">About us</Nav.Link>
+                  <Nav.Link href="#action2">Blog</Nav.Link>
+                  <Nav.Link href="#action2">Contact Us</Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+
+            <div className="d-flex align-items-center header-action">
+              <span className="pe-2 pe-md-3 me-2 me-md-3 border-end">
+                <i className="bi bi-person me-1 "></i>
+                <span className="d-none d-sm-inline-block">Login</span>
+              </span>
+              <span className="me-1 me-md-3">
+                <i className="bi bi-search me-1 "></i>
+                <span className="d-none d-sm-inline-block">Search</span>
+              </span>
+              <Navbar.Toggle className="ms-3" aria-controls={`offcanvasNavbar-expand-lg`} />
+            </div>
+          </Container>
+        </Navbar>
+      </Container>
+    </header>
+  );
 }
 
 export default Header;
