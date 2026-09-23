1c4446962fcf6e17334b985123ac9263bfebefd0
Hossein Douzandeh <douzandeh.k@gmail.com>
2026-09-24 00:06:40 +0330

    feat: render feature items from data

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import CategoryCard from "../../components/layout/cards/CategoryCard";
 import HeroSlider from "../../components/layout/Hero/HeroSlider";
-import { categories } from "../../constants/data";
+import { categories, features } from "../../constants/data";
 
 function Home() {
   return (
@@ -21,6 +21,24 @@ function Home() {
                   } mb-4`}
                 >
                   <CategoryCard cat={cat} index={index} />
+                </div>
+              );
+            })}
+            {features.map((val, index) => {
+              return (
+                <div
+                  className="feature-section col-lg-3 col-md-6 col-sm-6 col-12 mb-3 mb-md-0"
+                  key={index}
+                >
+                  <div className="feature-item d-flex align-items-center flex-column flex-sm-row">
+                    <div className="feature-icon me-sm-3 me-0 mb-sm-0">
+                      <i className={val.icon}></i>
+                    </div>
+                    <div className="feature-text text-center text-sm-start">
+                      <h5 className="fw-bold mb-0">{val.title}</h5>
+                      <p className="mb-0 text-muted">{val.desc}</p>
+                    </div>
+                  </div>
                 </div>
               );
             })}
